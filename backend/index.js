9e2d54be87bb5fcd45232efa1d9b98e8f8d3c41c
Omprakash <omdhanurkar444@gmail.com>
2023-01-28 17:34:29 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 
 const express = require('express');
 const app = express();
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const mysql = require("mysql2")  //mysql2 bcoz we have version 8.
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+
 
 const db = mysql.createConnection({
     host: "localhost",
@@ -17,20 +21,57 @@ app.get("/api/get", (req, res) => {
     const getData = "SELECT * FROM users";
     db.query(getData, (err, result) => {
         res.send(result)
+        //console.log(result)
+
+    })
+})
+app.post("/api/post", (req, res) => {
+    const { name, email, contact } = req.body;
+    const sqlInsert = "INSERT INTO users(name,email,contact) VALUES (?,?,?)";
+    db.query(sqlInsert, [name, email, contact], (error, result) => {
+        if (error) {
+            console.log(error)
+        }
+    })
+})
+
+app.delete("/api/remove/:id", (req, res) => {
+    const { id } = req.params;
+    const sqlRemove = "DELETE FROM users WHERE id =?";
+    db.query(sqlRemove, id, (error, result) => {
+        if (error) {
+            console.log(error)
+        }
 
     })
 })
 
-app.get("/", (req, res) => {
-    // const insertData ="INSERT INTO users (name,email,contact) VALUES('om','omdhanurkar@gmail.com',9898655326)";
-    // db.query(insertData,(err,result)=>{
-    //     console.log("err",err)
-    //     console.log("result",result)
-    //     res.send("heeloo");
+app.get("/api/get/:id", (req, res) => {
+    const { id } = req.params;
+    const getData = "SELECT * FROM users Where id = ?";
+    db.query(getData, id, (err, result) => {
+        if (err) {
+            console.log(err)
+        }
+        res.send(result)
+    });
+});
 
-    // })
-})
+app.put("/api/update/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, email, contact } = req.body;
+    const sqlUpdate = "UPDATE users SET name = ?,email = ?,contact = ? WHERE id = ?";
+    db.query(sqlUpdate, [name, email, contact, id], (error, result) => {
+        if (error) {
+            console.log(error)
+        }
+        res.send(result)
 
-app.listen(process.env.PORT || 3001, function () {
-    console.log('Express app running on port ' + (process.env.PORT || 3001))
+    });
+});
+
+
+
+app.listen(process.env.PORT || 5000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 5000))
 });
